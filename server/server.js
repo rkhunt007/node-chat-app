5a94844157e3346862d0dc41a7141254a00673fc
Rahul Khunt <rahul.khunt@einfochips.com>
2017-07-28 23:38:33 +0530

    Added socket io for 2 way communication

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@
 */
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const app = express();
 
 const publicPath = path.join(__dirname, "/../public");
@@ -10,6 +12,17 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, function() {
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+	console.log("New user connected");
+
+	socket.on('disconnect', () => {
+		console.log("User disconnected");
+	})
+})
+
+server.listen(PORT, function() {
 	console.log("Running app on port", PORT);
 })
