c5af77ed2d78fd106cdc6039077eff09526caa3d
Rahul Khunt <rkhunt007@gmail.com>
2017-09-24 18:20:16 +0530

    Added form to send messages

--- server/server.js
+++ server/server.js
@@ -27,9 +27,10 @@ io.on('connection', (socket) => {
 		console.log("User disconnected");
 	})
 
-	socket.on('createMessage', (message) => {
+	socket.on('createMessage', (message, callback) => {
 		console.log("createMessage", message);
 		io.emit('newMessage', generateMessage(message.from, message.text));
+		callback("This is from server");
 	})
 })
 
