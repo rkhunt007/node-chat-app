7f74da32c4bc1b50bef36f70cccdc66aae4e8f88
Rahul Khunt <rkhunt007@gmail.com>
2017-09-25 22:31:53 +0530

    timestamp using moment
    q
    :q
    wq

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const { generateMessage, generateLocationMessage } = require('./utils/message')
 const publicPath = path.join(__dirname, "/../public");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(publicPath));
 
